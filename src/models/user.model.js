fa7d0e04e16f73d5a6600d1a399036343e18b0f4
Kinza <kiinza.malik@gmail.com>
2024-05-26 10:34:57 +0500

    Register Controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose,{Schema} from mongoose;
+import mongoose,{Schema} from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
@@ -57,7 +57,7 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next()
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -92,14 +92,4 @@ userSchema.methods.generateRefreshToken = function(){
     )
 }
 
-
-
-
-
-
-
-
-
-
-
-export const UserSchema = mongoose.model("User",userSchema)
+export const User = mongoose.model("User",userSchema)
